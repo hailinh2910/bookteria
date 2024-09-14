461a24a6157e2b899f23259c2eac9a0c84bc3ce8
hailinh2910 <linhnhse170530@fpt.edu.vn>
2024-09-14 22:37:05 +0700

    update: #4 api gateway

--- profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -20,11 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserProfileController {
     UserProfileService userProfileService;
 
-    @PostMapping("/users")
-    UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request) {
-        return userProfileService.createProfile(request);
-    }
-
     @GetMapping("/users/{profileId}")
     UserProfileResponse getProfile(@PathVariable String profileId) {
         return userProfileService.getProfile(profileId);
